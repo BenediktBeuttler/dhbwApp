572931efade997604b8b3db4bf1e888f1f01c0c8
Tim <tim.reppenhagen@gmail.com>
2012-12-12 13:26:08 +0100

    Added Import/Export to class Exchange, finished all methods

---
+++ dhbwApp/src/wi2010d/dhbwapp/control/Exchange.java
@@ -0,0 +1,168 @@
+package wi2010d.dhbwapp.control;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.jdom2.JDOMException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import wi2010d.dhbwapp.model.Card;
+import wi2010d.dhbwapp.model.Stack;
+import wi2010d.dhbwapp.model.Tag;
+import wi2010d.dhbwapp.errorhandler.ErrorHandler;
+
+public class Exchange {
+
+	public boolean exportStack(Stack stack, String outputPath, String exportName)
+			throws Exception {
+		Document exportDoc = DocumentBuilderFactory.newInstance()
+				.newDocumentBuilder().newDocument();
+
+		Element stackElem = exportDoc.createElement("stack");
+		stackElem.setAttribute("name", stack.getStackName());
+		stackElem.setAttribute("isDynamicGenerated",
+				"" + stack.isDynamicGenerated());
+		if (stack.isDynamicGenerated()) {
+			for (Tag tag : stack.getDynamicStackTags()) {
+				Element tagElem = exportDoc.createElement("tag");
+				tagElem.setAttribute("name", tag.getTagName());
+				tagElem.setAttribute("totalCards", "" + tag.getTotalCards());
+				stackElem.appendChild(tagElem);
+			}
+		}
+
+		exportDoc.appendChild(stackElem);
+
+		for (Card card : stack.getCards()) {
+			Element cardElem = exportDoc.createElement("card");
+			cardElem.setAttribute("front", card.getCardFront());
+			cardElem.setAttribute("back", card.getCardBack());
+			cardElem.setAttribute("frontPic", card.getCardFrontPicture());
+			cardElem.setAttribute("backPic", card.getCardBackPicture());
+
+			for (Tag tag : card.getTags()) {
+				Element tagElem = exportDoc.createElement("tag");
+				tagElem.setAttribute("name", tag.getTagName());
+				cardElem.appendChild(tagElem);
+			}
+
+			stackElem.appendChild(cardElem);
+		}
+
+		TransformerFactory
+				.newInstance()
+				.newTransformer()
+				.transform(new DOMSource(exportDoc),
+						new StreamResult(outputPath + exportName + ".xml"));
+		return true;
+	}
+
+	public boolean exportCards(List<Card> cards, String outputPath,
+			String exportName) {
+		Document exportDoc;
+		try {
+			exportDoc = DocumentBuilderFactory.newInstance()
+					.newDocumentBuilder().newDocument();
+
+			Element rootElem = exportDoc.createElement("cardList");
+			for (Card card : cards) {
+				Element cardElem = exportDoc.createElement("card");
+				cardElem.setAttribute("front", card.getCardFront());
+				cardElem.setAttribute("back", card.getCardBack());
+				cardElem.setAttribute("frontPic", card.getCardFrontPicture());
+				cardElem.setAttribute("backPic", card.getCardBackPicture());
+
+				for (Tag tag : card.getTags()) {
+					Element tagElem = exportDoc.createElement("tag");
+					tagElem.setAttribute("name", tag.getTagName());
+					cardElem.appendChild(tagElem);
+				}
+
+				rootElem.appendChild(cardElem);
+			}
+
+			TransformerFactory
+					.newInstance()
+					.newTransformer()
+					.transform(new DOMSource(exportDoc),
+							new StreamResult(outputPath + exportName + ".xml"));
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			ErrorHandler.getInstance().handleError(
+					ErrorHandler.getInstance().EXPORT_ERROR);
+			return false;
+		}
+		return true;
+	}
+
+	public boolean importStack(String pathToXML) throws JDOMException,
+			IOException {
+		org.jdom2.Document importDoc = new org.jdom2.input.SAXBuilder()
+				.build(new File(pathToXML));
+
+		org.jdom2.Element rootElem = importDoc.getRootElement();
+
+		if (rootElem.getName().equals("stack")) {
+			boolean isDynamicGenerated;
+			String stackName = rootElem.getAttributeValue("name");
+
+			if (rootElem.getAttributeValue("isDynamicGenerated").equals("true")) {
+				isDynamicGenerated = true;
+			} else {
+				isDynamicGenerated = false;
+			}
+			new Stack(isDynamicGenerated, Stack.getNextStackID(), stackName, 0,
+					0, 0);
+
+			this.createCards(rootElem.getChildren());
+		}
+
+		else if (rootElem.getName().equals("cardList")) {
+			createCards(rootElem.getChildren());
+		} else {
+			ErrorHandler handler = ErrorHandler.getInstance();
+			handler.handleError(handler.IMPORT_ERROR);
+			return false;
+		}
+		return true;
+	}
+
+	private boolean createCards(List<org.jdom2.Element> cards) {
+		for (org.jdom2.Element cardElem : cards) {
+			String front = cardElem.getAttributeValue("front");
+			String back = cardElem.getAttributeValue("back");
+			String frontPic = cardElem.getAttributeValue("frontPic");
+			String backPic = cardElem.getAttributeValue("backPic");
+			List<Tag> tagList = new ArrayList<Tag>();
+			int foundTag = -1;
+			for (org.jdom2.Element tagElem : cardElem.getChildren()) {
+				// check if the Tag already exists
+				for (int i = 0; i < Tag.allTags.size(); i++) {
+					if (tagElem.getAttributeValue("name").equals(
+							Tag.allTags.get(i).getTagName())) {
+						foundTag = i;
+					}
+				}
+				// if it exists, add the found one
+				if (foundTag >= 0) {
+					tagList.add(Tag.allTags.get(foundTag));
+				} else {
+					tagList.add(new Tag(tagElem.getAttributeValue("name")));
+				}
+
+			}
+			new Card(front, back, frontPic, backPic, tagList);
+		}
+		return true;
+	}
+
+}
