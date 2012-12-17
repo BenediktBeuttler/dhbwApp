ba9325c32c2edd7d21131a065e53f8e90ce64b38
Tim <tim.reppenhagen@gmail.com>
2012-12-17 15:04:19 +0100

    Added Import/Export Screen

--- dhbwApp/src/wi2010d/dhbwapp/StartScreen.java
+++ dhbwApp/src/wi2010d/dhbwapp/StartScreen.java
@@ -10,33 +10,31 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class StartScreen extends Activity implements OnClickListener{
+public class StartScreen extends Activity implements OnClickListener {
 	Button learning, edit, admin, settings, statistic;
-	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.start_screen);
-		
-		learning = (Button)findViewById(R.id.btn_start_learning);
-		edit = (Button)findViewById(R.id.btn_start_help);
-		admin = (Button)findViewById(R.id.btn_start_admin);
-		settings = (Button)findViewById(R.id.btn_start_settings);
-		statistic = (Button)findViewById(R.id.btn_start_statistic);
+
+		learning = (Button) findViewById(R.id.btn_start_learning);
+		edit = (Button) findViewById(R.id.btn_start_help);
+		admin = (Button) findViewById(R.id.btn_start_admin);
+		settings = (Button) findViewById(R.id.btn_start_settings);
+		statistic = (Button) findViewById(R.id.btn_start_statistic);
 
 		learning.setOnClickListener(this);
 		edit.setOnClickListener(this);
 		admin.setOnClickListener(this);
 		settings.setOnClickListener(this);
 		statistic.setOnClickListener(this);
-		
-		//Initiate DB Manager, Database and load everything
+
+		// Initiate DB Manager, Database and load everything
 		Init init = Init.getInstance(getApplicationContext());
 		init.loadFromDB();
 
 	}
-
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -44,27 +42,25 @@ public class StartScreen extends Activity implements OnClickListener{
 		getMenuInflater().inflate(R.menu.start_screen, menu);
 		return true;
 	}
-	
 
 	@Override
 	public void onClick(View v) {
-				
+
 		switch (v.getId()) {
 		case R.id.btn_start_learning:
-			startActivity(new Intent (this, LearningChooseStackScreen.class));
+			startActivity(new Intent(this, LearningChooseStackScreen.class));
 			break;
 		case R.id.btn_start_help:
-			startActivity(new Intent (this, EditScreen.class));
+			startActivity(new Intent(this, EditScreen.class));
 			break;
-		case R.
-		id.btn_start_admin:
-			startActivity(new Intent (this, AdminScreen.class));
+		case R.id.btn_start_admin:
+			startActivity(new Intent(this, AdminScreen.class));
 			break;
 		case R.id.btn_start_settings:
-			startActivity(new Intent (this, SettingsScreen.class));
+			startActivity(new Intent(this, SettingsScreen.class));
 			break;
 		case R.id.btn_start_statistic:
-			startActivity(new Intent (this, StatisticsScreen.class));
+			startActivity(new Intent(this, StatisticsScreen.class));
 			break;
 
 		default:
@@ -72,7 +68,7 @@ public class StartScreen extends Activity implements OnClickListener{
 			error.handleError(1);
 			break;
 		}
-		
+
 	}
 
 }
