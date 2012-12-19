e2d60f4cc08225e8d4983ee31f784773d5112878
Benedikt Beuttler <benedikt.beuttler@gmail.com>
2012-12-19 15:09:12 +0100

    Progress/Manifest/StartScreen changed to implement loadingScreen

--- dhbwApp/src/wi2010d/dhbwapp/Progress.java
+++ dhbwApp/src/wi2010d/dhbwapp/Progress.java
@@ -1,15 +1,32 @@
 package wi2010d.dhbwapp;
 
-import android.os.Bundle;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import wi2010d.dhbwapp.control.Init;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ProgressBar;
 
 public class Progress extends Activity {
-
+	ProgressBar pb;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.progress_screen);
+
+		// Initiate DB Manager, Database and load everything
+		Init init = Init.getInstance(getApplicationContext());
+		init.loadFromDB();
+		
+		pb = (ProgressBar) findViewById(R.id.progress_bar_progress_screen);
+
+		Intent i = new Intent(getApplicationContext(), StartScreen.class);
+		startActivity(i);
+		
 	}
 
 	@Override
