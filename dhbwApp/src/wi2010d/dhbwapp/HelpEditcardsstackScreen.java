348812723b4b4868853b487b48352dee024707f8
WuttkeJ <jen.wuttke@gmail.com>
2012-12-19 13:24:57 +0100

    Help_layouts + activities added

---
+++ dhbwApp/src/wi2010d/dhbwapp/HelpEditcardsstackScreen.java
@@ -0,0 +1,15 @@
+package wi2010d.dhbwapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class HelpEditcardsstackScreen extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.help_editcardsstack_screen);
+	}
+
+}
