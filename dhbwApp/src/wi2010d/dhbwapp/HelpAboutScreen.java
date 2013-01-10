f26edbac61007fc5c9c15d18bd969103e5cadef7
Benedikt Beuttler <Benedikt Beuttler@BenediktBeuttle>
2013-01-10 21:48:43 +0100

    Added Rating funkction in help_about, and did some more stuff there

--- dhbwApp/src/wi2010d/dhbwapp/HelpAboutScreen.java
+++ dhbwApp/src/wi2010d/dhbwapp/HelpAboutScreen.java
@@ -2,17 +2,25 @@ package wi2010d.dhbwapp;
 
 
 import wi2010d.dhbwapp.errorhandler.ErrorHandlerFragment;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class HelpAboutScreen extends OnResumeActivity {
+public class HelpAboutScreen extends OnResumeActivity implements OnClickListener{
 
-	private TextView appName, version;
+	private TextView appName, version, feedback;
 	private ImageView knowitall;
+	private ImageButton email;
+	private RatingBar rating;
+	private float ratingNumer;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +30,33 @@ public class HelpAboutScreen extends OnResumeActivity {
 		appName = (TextView) findViewById(R.id.lbl_help_about_app_name);
 		version = (TextView) findViewById(R.id.lbl_help_about_vers);
 		knowitall = (ImageView) findViewById(R.id.img_help_about_knowitall);
+		feedback = (TextView) findViewById(R.id.lbl_help_about_feedback);
+		email = (ImageButton) findViewById(R.id.btn_help_about_email);
+		rating = (RatingBar)findViewById(R.id.ratingBar);
 
+		email.setOnClickListener(this);
 	}
 
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.btn_help_about_email:
+			ratingNumer = Float.valueOf(rating.getRating());
+			if(ratingNumer == 0){
+				Toast toast = Toast.makeText(
+						getApplicationContext(),
+						"Please give a rating first.",
+						Toast.LENGTH_SHORT);
+				toast.show();
+			}else{
+				//TODO: Thomas do it! Die Variable mit dem Rating is ratingNumer, aber als float
+				// kannst meine Mailadresse angeben: benedikt.beuttler@gmail.com
+			}
+			break;
+		default:
+			break;
+		}
+	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -38,10 +70,6 @@ public class HelpAboutScreen extends OnResumeActivity {
 		switch (item.getItemId()) {
 		case R.id.menu_start_screen:
 			startActivity(new Intent(this, StartScreen.class));
-			finish();
-			return true;
-		case R.id.menu_settings:
-			startActivity(new Intent(this, SettingsScreen.class));
 			finish();
 			return true;
 		default:
