4da7e362f43ef5f78eae5809527069916d028947
Tim <tim.reppenhagen@gmail.com>
2013-01-17 16:45:50 +0100

    Deleted unused import in init

--- dhbwApp/src/wi2010d/dhbwapp/control/Init.java
+++ dhbwApp/src/wi2010d/dhbwapp/control/Init.java
@@ -16,7 +16,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.AsyncTask;
-import android.util.Log;
 import android.widget.ProgressBar;
 
 /**
